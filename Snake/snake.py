b8aac4923b5dd30285cca618e9f9c35595e0ecf0
thlam1 <thlam@csumb.edu>
2018-04-08 16:48:20 -0700

    updating game code only

---
+++ Snake/snake.py
@@ -0,0 +1,151 @@
+from turtle import Turtle, Screen
+import random
+import time
+
+
+SIZE = 20
+
+class Square:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def drawself(self, turtle):
+
+        turtle.goto(self.x - SIZE // 2 - 1, self.y - SIZE // 2 - 1)
+
+        turtle.begin_fill()
+        for _ in range(4):
+            turtle.forward(SIZE - SIZE // 10)
+            turtle.left(90)
+        turtle.end_fill()
+
+class Food:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.is_blinking = True
+
+    def changelocation(self):
+        self.x = random.randint(0, SIZE) * SIZE - 200
+        self.y = random.randint(0, SIZE) * SIZE - 200
+
+    def drawself(self, turtle):
+        if self.is_blinking:
+            turtle.goto(self.x - SIZE // 2 - 1, self.y - SIZE // 2 - 1)
+            turtle.begin_fill()
+            for _ in range(4):
+                turtle.forward(SIZE - SIZE // 10)
+                turtle.left(90)
+            turtle.end_fill()
+
+    def changestate(self):
+        self.is_blinking = not self.is_blinking
+
+class Snake:
+    def __init__(self):
+        self.headposition = [SIZE, 0]
+        self.body = [Square(-SIZE, 0), Square(0, 0), Square(SIZE, 0)]
+        self.nextX = 1
+        self.nextY = 0
+        self.crashed = False
+        self.nextposition = [self.headposition[0] + SIZE * self.nextX, self.headposition[1] + SIZE * self.nextY]
+
+    def moveOneStep(self):
+        if Square(self.nextposition[0], self.nextposition[1]) not in self.body:
+            self.body.append(Square(self.nextposition[0], self.nextposition[1]))
+            del self.body[0]
+            self.headposition[0], self.headposition[1] = self.body[-1].x, self.body[-1].y
+            self.nextposition = [self.headposition[0] + SIZE * self.nextX, self.headposition[1] + SIZE * self.nextY]
+        else:
+            self.crashed = True
+
+    def moveup(self):
+        self.nextX, self.nextY = 0, 1
+
+    def moveleft(self):
+        self.nextX, self.nextY = -1, 0
+
+    def moveright(self):
+        self.nextX, self.nextY = 1, 0
+
+    def movedown(self):
+        self.nextX, self.nextY = 0, -1
+
+    def eatFood(self):
+        self.body.append(Square(self.nextposition[0], self.nextposition[1]))
+        self.headposition[0], self.headposition[1] = self.body[-1].x, self.body[-1].y
+        self.nextposition = [self.headposition[0] + SIZE * self.nextX, self.headposition[1] + SIZE * self.nextY]
+
+    def drawself(self, turtle):
+        for segment in self.body:
+            segment.drawself(turtle)
+
+class Game:
+    def __init__(self):
+        self.screen = Screen()
+        self.artist = Turtle(visible=False)
+        self.artist.up()
+        self.artist.speed("slowest")
+
+        self.snake = Snake()
+        self.food = Food(100, 0)
+        self.counter = 0
+        self.commandpending = False
+
+        self.screen.tracer(0)
+
+        self.screen.listen()
+        self.screen.onkey(self.snakedown, "Down")
+        self.screen.onkey(self.snakeright, "Right")
+        self.screen.onkey(self.snakeleft, "Left")
+        self.screen.onkey(self.snakeup, "Up")
+
+    def nextFrame(self):
+        self.artist.clear()
+
+        if (self.snake.nextposition[0], self.snake.nextposition[1]) == (self.food.x, self.food.y):
+            self.snake.eatFood()
+            self.food.changelocation()
+        else:
+            self.snake.moveOneStep()
+
+        if self.counter == 10:
+            self.food.changestate()
+            self.counter = 0
+        else:
+            self.counter += 1
+
+        self.food.drawself(self.artist)
+        self.snake.drawself(self.artist)
+        self.screen.update()
+        self.screen.ontimer(lambda: self.nextFrame(), 100)
+
+    def snakeup(self):
+        if not self.commandpending:
+            self.commandpending = True
+            self.snake.moveup()
+            self.commandpending = False
+
+    def snakedown(self):
+        if not self.commandpending:
+            self.commandpending = True
+            self.snake.movedown()
+            self.commandpending = False
+
+    def snakeleft(self):
+        if not self.commandpending:
+            self.commandpending = True
+            self.snake.moveleft()
+            self.commandpending = False
+
+    def snakeright(self):
+        if not self.commandpending:
+            self.commandpending = True
+            self.snake.moveright()
+            self.commandpending = False
+
+game = Game()
+screen = Screen()
+screen.ontimer(lambda: game.nextFrame(), 100)
+screen.mainloop()
