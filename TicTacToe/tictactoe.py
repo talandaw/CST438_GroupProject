88e685edf9554d83080edb17717cc411f0a7c80a
thlam1 <thlam@csumb.edu>
2018-04-08 17:44:09 -0700

    update tictactoe game v2
    update tictactoe game v2

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -1,100 +1,270 @@
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-print("TIC TAC TOE!")
-print("The ROWS & COLUMNS start at 1,1. The first number is the ROW and the second is the COLUMN.")
-print("Enter your selection in the following format, ex: 1,2 or 3,2 or 2,3")
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-print("\n")
+import pygame
+from pygame.locals import *
+
+
+# Create the constants (go ahead and experiment with different values)
+BOARDWIDTH = 3  # number of columns in the board
+BOARDHEIGHT = 3 # number of rows in the board
+TILESIZE = 100
+WINDOWWIDTH = 480
+WINDOWHEIGHT = 480
+FPS = 30
+BLANK = None
+
+#                 R    G    B
+BLACK =         (  0,   0,   0)
+WHITE =         (255, 255, 255)
+BRIGHTBLUE =    (  0,  50, 255)
+DARKTURQUOISE = (  3,  54,  73)
+GREEN =         (  0, 204,   0)
+
+BGCOLOR = DARKTURQUOISE
+TILECOLOR = GREEN
+TEXTCOLOR = WHITE
+BORDERCOLOR = BRIGHTBLUE
+BASICFONTSIZE = 20
+
+BUTTONCOLOR = WHITE
+BUTTONTEXTCOLOR = BLACK
+MESSAGECOLOR = WHITE
+
+BLANK = 10
+PLAYER_O = 11
+PLAYER_X = 21
+
+
+PLAYER_O_WIN = PLAYER_O * 3
+PLAYER_X_WIN = PLAYER_X * 3
+
+CONT_GAME         = 10
+DRAW_GAME         = 20
+QUIT_GAME         = 30
+
+XMARGIN = int((WINDOWWIDTH - (TILESIZE * BOARDWIDTH + (BOARDWIDTH - 1))) / 2)
+YMARGIN = int((WINDOWHEIGHT - (TILESIZE * BOARDHEIGHT + (BOARDHEIGHT - 1))) / 2)
+
+choice = 0
+
+
+def drawBoard(board, message):
+    DISPLAYSURF.fill(BGCOLOR)
+    if message:
+        textSurf, textRect = makeText(message, MESSAGECOLOR, BGCOLOR, 5, 5)
+        DISPLAYSURF.blit(textSurf, textRect)
+
+    for tilex in range(3):
+        for tiley in range(3):
+            if board[tilex*3+tiley] != BLANK:
+                drawTile(tilex, tiley, board[tilex*3+tiley])
+
+    left, top = getLeftTopOfTile(0, 0)
+    width = BOARDWIDTH * TILESIZE
+    height = BOARDHEIGHT * TILESIZE
+    pygame.draw.rect(DISPLAYSURF, BORDERCOLOR, (left - 5, top - 5, width + 11, height + 11), 4)
+
+    DISPLAYSURF.blit(NEW_SURF, NEW_RECT)
+    DISPLAYSURF.blit(NEW_SURF2, NEW_RECT2)
+
+
+def getLeftTopOfTile(tileX, tileY):
+    left = XMARGIN + (tileX * TILESIZE) + (tileX - 1)
+    top = YMARGIN + (tileY * TILESIZE) + (tileY - 1)
+    return (left, top)
+
+
+def makeText(text, color, bgcolor, top, left):
+    textSurf = BASICFONT.render(text, True, color, bgcolor)
+    textRect = textSurf.get_rect()
+    textRect.topleft = (top, left)
+    return (textSurf, textRect)
+
+def check_win_game(board):
+    def check_draw_game():
+        return sum(board)%10 == 9
+
+    def check_horizontal(player):
+        for i in [0, 3, 6]:
+            if sum(board[i:i+3]) == 3 * player:
+                return player
+
+    def check_vertical(player):
+        for i in range(3):
+            if sum(board[i::3]) == 3 * player:
+                return player
+
+    def check_diagonals(player):
+        if (sum(board[0::4]) == 3 * player) or (sum(board[2:7:2]) == 3 * player):
+            return player
+
+    for player in [PLAYER_X, PLAYER_O]:
+        if any([check_horizontal(player), check_vertical(player), check_diagonals(player)]):
+            return player
+
+    return DRAW_GAME if check_draw_game() else CONT_GAME
+
+
+def unit_score(winner, depth):
+    if winner == DRAW_GAME:
+        return 0
+    else:
+        return 10 - depth if winner == PLAYER_X else depth - 10
+
+
+def get_available_step(board):
+    return [i for i in range(9) if board[i] == BLANK]
 
 
 
-def game_winner(game):
-    for i in range(3):
-        row = set(game[i])
-        if len(row) == 1 and game[i][2] != 0:
-            return game[i][2]
-
-    for j in range(3):
-        column = set([game[0][j], game[1][j], game[2][j]])
-        if len(column) == 1 and game[1][j] != 0:
-            return game[1][j]
-
-    diag1 = set([game[0][0], game[1][1], game[2][2]])
-    diag2 = set([game[0][2], game[1][1], game[2][0]])
-    if len(diag1) == 1 or len(diag2) == 1 and game[1][1] != 0:
-        return game[1][1]
-
-    return "No One"
-
-def start_game(game):
-    print("\n")
-    for i in range(3):
-        print("  |  ".join(str(x) for x in game[i]))
-        if i < 2:
-            print("-------------")
 
 
+def update_state(board, step, depth):
+    board = list(board)
+    board[step] = PLAYER_X if depth % 2 else PLAYER_O
+    return board
 
-if __name__ == "__main__":
 
-    ans = 'y'
-    count_x = 0
-    count_o = 0
+def update_board(board, step, player):
+    board[step] = player
 
-    while ans != 'n':
-        game = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 
-        start_game(game)
+def change_to_player(player):
+    if player == PLAYER_O:
+        return 'O'
+    elif player == PLAYER_X:
+        return 'X'
+    elif player == BLANK:
+        return '-'
 
-        count = 0
-        chance = True
 
-        while chance:
-            spot = input("Enter your selection in the following format, ex: 1,2 or 3,2 or 2,3: ")
+def drawTile(tilex, tiley, symbol, adjx=0, adjy=0):
+    left, top = getLeftTopOfTile(tilex, tiley)
+    pygame.draw.rect(DISPLAYSURF, TILECOLOR, (left + adjx, top + adjy, TILESIZE, TILESIZE))
+    textSurf = BASICFONT.render(symbol_to_str(symbol), True, TEXTCOLOR)
+    textRect = textSurf.get_rect()
+    textRect.center = left + int(TILESIZE / 2) + adjx, top + int(TILESIZE / 2) + adjy
+    DISPLAYSURF.blit(textSurf, textRect)
 
-            spot = spot.split(",")  # gives strings
 
-            row = int(spot[0]) - 1
-            column = int(spot[1]) - 1
+def symbol_to_str(symbol):
+    if symbol == PLAYER_O:
+        return 'O'
+    elif symbol == PLAYER_X:
+        return 'X'
 
-            if count % 2 == 0:
-                print("\nPlayer 1's turn!")
-                if game[row][column] == " ":
-                    game[row][column] = 'X'
+
+def getSpotClicked(x, y):
+    for tileX in range(3):
+        for tileY in range(3):
+            left, top = getLeftTopOfTile(tileX, tileY)
+            tileRect = pygame.Rect(left, top, TILESIZE, TILESIZE)
+            if tileRect.collidepoint(x, y):
+                return (tileX, tileY)
+    return None
+
+
+def board_to_step(spotx, spoty):
+    return spotx * 3 + spoty
+
+
+def check_move_legal(coords, board):
+    step = board_to_step(*coords)
+    return board[step] == BLANK
+
+def minmax(board, depth):
+    global choice
+    result = check_win_game(board)
+    if result != CONT_GAME:
+        return unit_score(result, depth)
+
+    depth += 1
+    scores = []
+    steps = []
+
+    for step in get_available_step(board):
+        score = minmax(update_state(board, step, depth), depth)
+        scores.append(score)
+        steps.append(step)
+
+    if depth % 2 == 1:
+        max_value_index = scores.index(max(scores))
+        choice = steps[max_value_index]
+        return max(scores)
+    else:
+        min_value_index = scores.index(min(scores))
+        choice = steps[min_value_index]
+        return min(scores)
+
+
+
+def main():
+    global FPSCLOCK, DISPLAYSURF, BASICFONT, NEW_SURF, NEW_RECT, NEW_SURF2, NEW_RECT2
+    two_player = False #by default false
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    pygame.display.set_caption('Tic-Tac-Toe')
+    BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
+    NEW_SURF, NEW_RECT = makeText('vs AI', TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 60)
+    NEW_SURF2, NEW_RECT2 = makeText('vs Human', TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 240, WINDOWHEIGHT - 60)
+    board = [BLANK] * 9
+    game_over = False
+    x_turn = True
+    msg = "Tic-Tac-Toe"
+    drawBoard(board, msg)
+    pygame.display.update()
+
+    while True:
+        coords = None
+        for event in pygame.event.get():
+            if event.type == MOUSEBUTTONUP:
+                coords = getSpotClicked(event.pos[0], event.pos[1])
+                if not coords and NEW_RECT.collidepoint(event.pos):
+                    board = [BLANK] * 9
+                    game_over = False
+                    msg = "Ticky - Unbeatable Tic Tac Toe AI"
+                    drawBoard(board, msg)
+                    pygame.display.update()
+                    two_player = False
+                if not coords and NEW_RECT2.collidepoint(event.pos):
+                    board = [BLANK] * 9
+                    game_over = False
+                    msg = "Ticky - Unbeatable Tic Tac Toe AI"
+                    drawBoard(board, msg)
+                    pygame.display.update()
+                    two_player = True
+        if coords and check_move_legal(coords, board) and not game_over:
+            if two_player:
+                next_step = board_to_step(*coords)
+                if x_turn:
+                    update_board(board, next_step, PLAYER_X)
+                    x_turn = False
                 else:
-                    print("That spot is already taken. Try another spot!")
-                    count -= 1
-                start_game(game)
+                    update_board(board, next_step, PLAYER_O)
+                    x_turn = True
+                drawBoard(board, msg)
+                pygame.display.update()
+
+            if not two_player:
+                next_step = board_to_step(*coords)
+                update_board(board, next_step, PLAYER_X)
+                drawBoard(board, msg)
+                pygame.display.update()
+                minmax(board, 0)
+                update_board(board, choice, PLAYER_O)
+
+            result = check_win_game(board)
+            game_over = (result != CONT_GAME)
+
+            if result == PLAYER_X:
+                msg = "X wins!"
+            elif result == PLAYER_O:
+                msg = "O wins!"
+            elif result == DRAW_GAME:
+                msg = "Draw game"
+
+            drawBoard(board, msg)
+            pygame.display.update()
 
 
-            else:
-                print("\nPlayer 2's turn!")
-                if game[row][column] == " ":
-                    game[row][column] = 'O'
-                else:
-                    print("That spot is already taken. Try another spot!")
-                    count -= 1
-                start_game(game)
-
-            count += 1
-
-            if " " in game[0] or " " in game[1] or " " in game[2]:
-                chance = True
-            else:
-                chance = False
-                print("Tie Game!")
-
-            if game_winner(game) == 'X':
-                print("The winner is ", game_winner(game))
-                count_x += 1
-                break
-            elif game_winner(game) == 'O':
-                print("The winner is ", game_winner(game))
-                count_o += 1
-                break
-            else:
-                print("No winner yet, please continue.")
-
-        ans = input("Play Again? [Y/N]): ")
-
-    print("Good game!")
-    print("Total Score for this session: \n\nPlayer 1: %s \nPlayer 2: %s" % (count_x, count_o))
+if __name__ == '__main__':
+    main()
